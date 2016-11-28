8833f1f122f5da25b3f931c482889c89528958ab
winterScoming <kimura@live.it>
2016-11-28 02:29:57 -0300

    Add files via upload

--- hash.cpp
+++ hash.cpp
@@ -1,78 +1,72 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-class hashClass
+#define tableSize 10
+
+int HashF(string key){
+	int hash=0;
+
+	for (int i = 0; i < key.length(); i++)
+	{
+		hash = hash + (int)key[i];
+		// cout<<"hash : "<<hash<<endl;
+	}
+	return hash % tableSize;
+}
+
+
+string String_vector(string s){
+	string x;
+	istringstream iss(s);
+	iss >> x;
+	return x;
+}
+
+class Palavras
 {
 private:
-	static const int tableSize = 10;
-	struct item
-	{
-		string name="empty";
-		string drink="empty";
-		item* next = NULL;
-	};
-
-	item HashTable[tableSize];
+	string palavra;
 public:
-	hashClass(){
-/*		for (int i = 0; i < tableSize; i++)
-		{
-			HashTable[i] = new item;
-			HashTable[i]->name = "empty";
-			HashTable[i]->drink = "empty";
-			HashTable[i]->next = NULL;
-		}*/
+	Palavras()=default;
+	Palavras(string p){
+		palavra = p;
 	}
-
-	int HashF(string key){
-		int hash=0;
-	
-		for (int i = 0; i < key.length(); i++)
-		{
-			hash = hash + (int)key[i];
-			// cout<<"hash : "<<hash<<endl;
-		}
-		return hash % tableSize;
+	void setPalavras(string p){
+		palavra = p;
 	}
-/*	void AddItem(string name, string drink){
-		int index = HashF(name);
-		if(HashTable[index].name == "empty"){
-			HashTable[index].name = name;
-			HashTable[index].drink = drink;
-		}
-		else{
-			item& Ptr = HashTable[index]; //usei &, invés de *... ver no que dá depois
-			item* n = new item;
-			n->name = name;
-			n->drink = drink;
-			n->next = NULL;
-			while(Ptr->next !=NULL){
-				Prt = Prt->next;
-			}
-
-		}
-	}*/
+	string getPalavras(){
+		return palavra;
+	}
 
 };
 
 string lerPalavras(){
-	string palavras;
+	string palavra;
 	fstream in;
 	in.open("palavras.txt");
 	while(!in.eof()){
-		getline (in,palavras);
-		//cout<<palavras<<endl;
-		return palavras;
+		getline (in,palavra);
+		// cout<<palavra<<endl;
+		return palavra;
 	}
 }
-
 int main(){
-	string palavras = lerPalavras();
-	cout<<palavras;
+	vector<Palavras> v;
 	
-	hashClass obj;
-	cout<<obj.HashF("Renan Kimura")<<endl;
-	
+
+	for (int i = 0; i < tableSize; i++)
+	{
+		Palavras p;
+		string s1 = String_vector(lerPalavras());
+		p.setPalavras(s1);
+		v[i]=p;
+
+	}
+
+	cout<<v[1].getPalavras();
+
 	system("PAUSE");
 }
